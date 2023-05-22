c1f7337c2186040a215984628773d2d6d2d77964
banchipena <ipena@tecnogonia.cl>
2023-05-22 20:51:28 -0300

    Fin de la seccion 4 de fundamentos

---
+++ assets/js/bases/switch.js
@@ -0,0 +1,19 @@
+
+
+const dia = 3;
+
+switch (dia) {
+    case 0:
+        console.log('Domingo');
+        break;
+    case 1:
+        console.log('Lunes');
+        break;
+    case 2:
+        console.log('Martes');
+        break;
+
+    default:
+        console.log('No es ni lunes,  ni martes, ni domingo')
+        break;
+}
